0a016de7d67c790348e20844aea31b34c4732848
SaribJawad <msssarib12@gmail.com>
2025-03-23 07:28:10 +0500

    Removed dist folder from version control

--- server/dist/routes/room.route.js
+++
@@ -1,9 +0,0 @@
-import { getAllRooms, getSongQueue, getRoom, } from "../controllers/room.controller.js";
-import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-const router = Router();
-// router.post("/create-room", verifyJWT, createRoom);
-router.get("/get-all-rooms", verifyJWT, getAllRooms);
-router.route("/:roomId").get(verifyJWT, getRoom);
-router.get("/song-queue/:roomId", verifyJWT, getSongQueue);
-export default router;
--- server/dist/utils/extractYoutubeId.js
+++
@@ -1,4 +0,0 @@
-export function extractYouTubeID(url) {
-    const match = url.match(/^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/);
-    return match ? match[1] : null;
-}
